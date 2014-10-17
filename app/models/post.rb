e31389cc0c4e57e6f273cdf085767d5378448c71
Nick Leli <nickleli@ENG001570.local>
2014-10-16 21:39:51 -0700

    full file upload

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ActiveRecord::Base
+has_many :comments, dependent: :destroy
+validates :title, presence: true,
+                    length: { minimum: 5 }
+					
+validates :text, presence: true,
+	length: { minimum: 5 }
+	
+mount_uploader :image, ImageUploader
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'ece70043e89fbd3a84b27d69e3c03e6abec5fdc63a53fd8eb3e6413308d1aa8a2c4356414d230f66b9272c5d7c91606049944583d48fb1b314e4462bb5102d39'
